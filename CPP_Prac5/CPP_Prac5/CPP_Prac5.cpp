f7b09c77d3457c52f6354a3939870f1121effe1b
Billiamd1012 <william.darker2003@gmail.com>
2025-04-03 15:39:30 +1030

    Made cMake project

---
+++ CPP_Prac5/CPP_Prac5/CPP_Prac5.cpp
@@ -0,0 +1,12 @@
+﻿// CPP_Prac5.cpp : Defines the entry point for the application.
+//
+
+#include "CPP_Prac5.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
